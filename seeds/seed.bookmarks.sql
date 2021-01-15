37686570d5c6ac5b9c683c6b663d1939999aa04f
Austin File <austinfile510@gmail.com>
2021-01-15 10:46:10 -0800

    refactored to support inserting bookmarks into db

--- seeds/seed.bookmarks.sql
+++ seeds/seed.bookmarks.sql
@@ -2,60 +2,60 @@ insert into bookmarks (title, url, rating, description)
 values (
         'Thinkful',
         'https://www.thinkful.com',
-        5,
+        '5',
         'Think outside the classroom'
     ),
     (
         'Google',
         'https://www.google.com',
-        5,
+        '5',
         'Where we find everything else'
     ),
     (
         'MDN',
         'https://developer.mozilla.org',
-        5,
+        '5',
         'The only place to find web documentation'
     ),
     (
         'Reddit',
         'https://www.reddit.com',
-        1,
+        '1',
         'Where to find nothing of value'
     ),
     (
         'Twitter',
         'https://www.twitter.com',
-        3,
+        '3',
         'Say anything in 280 characters'
     ),
     (
         'Facebook',
         'https://www.facebook.com',
-        2,
+        '2',
         'Where to connect with friends, family, and weird people from your high school'
     ),
     (
         'CNN',
         'https://www.cnn.com',
-        3,
+        '3',
         'Breaking news and videos'
     ),
     (
         'Github',
         'https://www.github.com',
-        5,
+        '5',
         'The best place to store and share your programming work'
     ),
     (
         'Amazon',
         'https://www.amazon.com',
-        4,
+        '4',
         'Shop for anything in a vast online store'
     ),
     (
         'eBay',
         'https://www.ebay.com',
-        5,
+        '5',
         'Bid, buy, and sell online'
     );
